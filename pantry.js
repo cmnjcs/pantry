1ab684654414668c39e977e8d23d121e10ebe78d
Kweku Adams <javaman9@gmail.com>
2014-05-02 13:55:43 -0400

    commented out unused template code

--- pantry.js
+++ pantry.js
@@ -1,15 +1,15 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to pantry.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+//  Template.hello.greeting = function () {
+//    return "Welcome to pantry.";
+//  };
+//
+//  Template.hello.events({
+//    'click input': function () {
+//      // template data, if any, is available in 'this'
+//      if (typeof console !== 'undefined')
+//        console.log("You pressed the button");
+//    }
+//  });
 }
 
 if (Meteor.isServer) {
