a00147e253fdedb5f1dddfc29247b3e963efcff0
sedris <sredris@gmail.com>
2014-05-04 13:41:20 -0400

    trying a hacky way to change templates when clicking links. can go to home, add, inventory, and trends

--- pantry.js
+++ pantry.js
@@ -1,15 +1,53 @@
-if (Meteor.isClient) {
-//  Template.hello.greeting = function () {
-//    return "Welcome to pantry.";
-//  };
-//
-//  Template.hello.events({
-//    'click input': function () {
-//      // template data, if any, is available in 'this'
-//      if (typeof console !== 'undefined')
-//        console.log("You pressed the button");
-//    }
-//  });
+if (Meteor.isClient) { 
+  var resetAll = function() {
+  Session.set('isHome', false);
+	Session.set('isAdd', false);
+	Session.set('isCamera', false);
+	Session.set('isInventory', false);
+	Session.set('isTrends', false);
+ };
+ 
+ resetAll();
+ Session.set('isHome', true);
+
+ Template.navbar.events({
+   'click .homeLink': function () {
+     if (typeof console !== 'undefined')
+			 resetAll();
+			 Session.set('isHome', true);
+   },
+   'click .addLink': function () {
+     if (typeof console !== 'undefined')
+			 resetAll();
+			 Session.set('isAdd', true);
+   },
+   'click .inventoryLink': function () {
+     if (typeof console !== 'undefined')
+			 resetAll();
+			 Session.set('isInventory', true);
+   },
+   'click .trendsLink': function () {
+     if (typeof console !== 'undefined')
+			 resetAll();
+			 Session.set('isTrends', true);
+   }
+ });
+ 
+ Template.main.isHome = function () {
+  return Session.get('isHome');
+	}
+	Template.main.isAdd = function () {
+		return Session.get('isAdd');
+	}
+	Template.main.isCamera = function () {
+		return Session.get('isCamera');
+	}
+	Template.main.isInventory = function () {
+		return Session.get('isInventory');
+	}
+	Template.main.isTrends = function () {
+		return Session.get('isTrends');
+	}
 }
 
 if (Meteor.isServer) {
