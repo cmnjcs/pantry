6123ed8124162a6f4c188b7e240a79cdc51b3066
Kweku Adams <javaman9@gmail.com>
2014-05-05 15:48:45 -0400

    made navigation methods client only

--- pantry.js
+++ pantry.js
@@ -1,22 +1,57 @@
-if (Meteor.isClient) { 
- Meteor.call('resetAll');
- Session.set('isHome', true);
+if (Meteor.isClient) {
+    Meteor.call('resetAll');
+    Session.set('isHome', true);
 
- Template.main.isHome = function () {
-  return Session.get('isHome');
-	}
-	Template.main.isAdd = function () {
-		return Session.get('isAdd');
-	}
-	Template.main.isCamera = function () {
-		return Session.get('isCamera');
-	}
-	Template.main.isInventory = function () {
-		return Session.get('isInventory');
-	}
-	Template.main.isTrends = function () {
-		return Session.get('isTrends');
-	}
+    Template.main.isHome = function () {
+        return Session.get('isHome');
+    }
+    Template.main.isAdd = function () {
+        return Session.get('isAdd');
+    }
+    Template.main.isCamera = function () {
+        return Session.get('isCamera');
+    }
+    Template.main.isInventory = function () {
+        return Session.get('isInventory');
+    }
+    Template.main.isTrends = function () {
+        return Session.get('isTrends');
+    }
+
+    Meteor.methods({
+        resetAll: function() {
+            Session.set('isHome', false);
+            Session.set('isAdd', false);
+            Session.set('isCamera', false);
+            Session.set('isInventory', false);
+            Session.set('isTrends', false);
+        },
+
+        gotoAdd: function() {
+            Meteor.call('resetAll');
+            Session.set('isAdd', true);
+        },
+
+        gotoHome: function() {
+            Meteor.call('resetAll');
+            Session.set('isHome', true);
+        },
+
+        gotoCamera: function() {
+            Meteor.call('resetAll');
+            Session.set('isCamera', true);
+        },
+
+        gotoInventory: function() {
+            Meteor.call('resetAll');
+            Session.set('isInventory', true);
+        },
+
+        gotoTrends: function() {
+            Meteor.call('resetAll');
+            Session.set('isTrends', true);
+        }
+    })
 }
 
 if (Meteor.isServer) {
@@ -24,38 +59,3 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
-
-Meteor.methods({
-    resetAll: function() {
-        Session.set('isHome', false);
-        Session.set('isAdd', false);
-        Session.set('isCamera', false);
-        Session.set('isInventory', false);
-        Session.set('isTrends', false);
-    },
-
-    gotoAdd: function() {
-        Meteor.call('resetAll');
-        Session.set('isAdd', true);
-    },
-
-    gotoHome: function() {
-        Meteor.call('resetAll');
-        Session.set('isHome', true);
-    },
-
-     gotoCamera: function() {
-        Meteor.call('resetAll');
-        Session.set('isCamera', true);
-    },
-
-     gotoInventory: function() {
-        Meteor.call('resetAll');
-        Session.set('isInventory', true);
-    },
-
-     gotoTrends: function() {
-        Meteor.call('resetAll');
-        Session.set('isTrends', true);
-    }
-})
