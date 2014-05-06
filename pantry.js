011c7ada59dfada2a779a680e1d9ce43831b2805
Kweku Adams <javaman9@gmail.com>
2014-05-05 22:31:21 -0400

    removed unused code

--- pantry.js
+++ pantry.js
@@ -1,38 +1,7 @@
+Items = new Meteor.Collection("items");
+
 if (Meteor.isClient) {
-    Meteor.methods({
-        resetAll: function() {
-            Session.set('isHome', false);
-            Session.set('isAdd', false);
-            Session.set('isCamera', false);
-            Session.set('isInventory', false);
-            Session.set('isTrends', false);
-        },
 
-        gotoAdd: function() {
-            Meteor.call('resetAll');
-            Session.set('isAdd', true);
-        },
-
-        gotoHome: function() {
-            Meteor.call('resetAll');
-            Session.set('isHome', true);
-        },
-
-        gotoCamera: function() {
-            Meteor.call('resetAll');
-            Session.set('isCamera', true);
-        },
-
-        gotoInventory: function() {
-            Meteor.call('resetAll');
-            Session.set('isInventory', true);
-        },
-
-        gotoTrends: function() {
-            Meteor.call('resetAll');
-            Session.set('isTrends', true);
-        }
-    })
 }
 
 if (Meteor.isServer) {
