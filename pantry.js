55fb9ef4a8e5d86ce2483faae81e9909766e2081
sedris <sredris@gmail.com>
2014-05-05 23:24:00 -0400

    added dummy items to inventory to display

--- pantry.js
+++ pantry.js
@@ -1,12 +1,36 @@
 Items = new Meteor.Collection("items");
 
 if (Meteor.isClient) {
-
+  Template.inventory.items = function () {
+    return Items.find({});
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+		if (Items.find().count() === 0) {
+			Items.insert({
+				name: "apple", 
+				exp_date: "2014-04-16", 
+				quantity: 1, 
+				ppi: .9, 
+				status: "stock",
+				date_removed: "",
+				type: "fruit",
+				date_acquired: "2014-04-10",
+				img_src: "images/apple.jpg"});
+			Items.insert({
+				name: "banana", 
+				exp_date: "2014-04-17", 
+				quantity: 2, 
+				ppi: .9, 
+				status: "stock",
+				date_removed: "",
+				type: "fruit",
+				date_acquired: "2014-04-10",
+				img_src: "images/banana.jpg"});
+		}
   });
 }
 
